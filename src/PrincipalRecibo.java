b2266ae0766113f87d42bfc5291b4c4bc757f086
Ricardo Rivas <richistron@gmail.com>
2023-08-29 20:41:14 -0700

    Fixing missing instructions

--- src/PrincipalRecibo.java
+++ src/PrincipalRecibo.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
 
 public class PrincipalRecibo {
-    private final ArrayList<Recibo> recibos = new ArrayList();
+    private final ArrayList<Recibo> recibos = new ArrayList<>();
 
     public PrincipalRecibo() {
-        this.recibos.add(new Recibo(100.20, "Pago Renta"));
-        this.recibos.add(new Recibo(99.90, "Netflix"));
+        this.recibos.add(new Recibo("Ricardo", 100.20, "Pago Renta"));
+        this.recibos.add(new Recibo("Cornelio", 99.90, "Netflix"));
     }
 
     public void ImprimirDatos() {
         for (int i = 0; i < this.recibos.size(); i++) {
             System.out.printf("El recibo número %s es:%n", i);
-            System.out.printf("el concepto es: %s%n", this.recibos.get(i).ConceptoPago);
-            System.out.printf("la cantidad es: %s%n", this.recibos.get(i).CantidadPago);
+            this.recibos.get(i).ImprimirDatos();
         }
     }
 }
--- src/Recibo.java
+++ src/Recibo.java
@@ -1,9 +1,17 @@
 public class Recibo {
-    String ConceptoPago;
-    Double CantidadPago;
+    private final String Nombre;
+    private final Double CantidadPago;
+    private final String ConceptoPago;
 
-    public Recibo(Double cantidad, String concepto) {
+    public Recibo(String nombre, Double cantidad, String concepto) {
+        Nombre = nombre;
         ConceptoPago = concepto;
         CantidadPago = cantidad;
+    }
+
+    public void ImprimirDatos() {
+        System.out.printf("el nombre es: %s%n", this.Nombre);
+        System.out.printf("el concepto es: %s%n", this.ConceptoPago);
+        System.out.printf("la cantidad es: %s%n", this.CantidadPago);
     }
 }
